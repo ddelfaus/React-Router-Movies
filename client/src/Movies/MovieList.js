a60e8cb0bdfea3a3fa27fcd8f3dc2afb5972e59b
ddelfaus <ddelfaus@gmail.com>
2019-10-14 16:48:15 -0400

    First Commit

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -8,7 +9,9 @@ const MovieList = props => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
+          console.log(response)
           setMovies(response.data);
+          
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -21,6 +24,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link to ={`movie/:id`}
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
